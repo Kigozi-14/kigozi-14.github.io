b28cc8501fa1ed15b94b9b9153b9225153a8f749
kigozi-14 <kigozie14@gmail.com>
2024-06-15 08:41:51 +0100

    i have found means of getting the current date and time via my html form ..  its working well

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/posting')
+def post():
+    return render_template('posting.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        my_date = request.form['currentDate']
+        my_description = request.form['description']
+        my_image= request.files['image_file'].read()
+        photo_name = request.files['image_file']
+        image_name = photo_name.filename
+        print(my_date, image_name, my_description)
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
