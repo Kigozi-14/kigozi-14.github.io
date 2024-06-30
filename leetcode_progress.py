5069ac4964debb2c2ca4cb0f653905d0f06b5fe2
kigozi-14 <kigozie14@gmail.com>
2024-06-30 05:40:03 +0100

    we are progressing smoothly   i cant type explain everything

--- leetcode_progress.py
+++ leetcode_progress.py
@@ -2,6 +2,7 @@ import requests
 
 def my_data(my_name):
     
+    lis = []
 
     # Define the endpoint and headers for the request
     url = "https://leetcode.com/graphql"
@@ -40,26 +41,23 @@ def my_data(my_name):
 
     # Make the POST request to the LeetCode GraphQL API
     response = requests.post(url, json=data, headers=headers)
-    lis = []
     # Check if the request was successful
     if response.status_code == 200:
         response_data = response.json()
 
-        # Check if matchedUser and submitStatsGlobal are not None
-        matched_user = response_data.get('data', {}).get('matchedUser', None)
-        if matched_user and matched_user.get('submitStatsGlobal', None):
-            ac_submission_num = matched_user['submitStatsGlobal'].get('acSubmissionNum', None)
-            if ac_submission_num:
-                total_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'All'), 0)
-                easy_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Easy'), 0)
-                medium_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Medium'), 0)
-                hard_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Hard'), 0)
-                lis.append(easy_solved)
-                lis.append(medium_solved)
-                lis.append(hard_solved)
-                lis.append(total_solved)
-                return lis
+    # Check if matchedUser and submitStatsGlobal are not None
+    matched_user = response_data.get('data', {}).get('matchedUser', None)
+    if matched_user and matched_user.get('submitStatsGlobal', None):
+        ac_submission_num = matched_user['submitStatsGlobal'].get('acSubmissionNum', None)
+        if ac_submission_num:
+            total_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'All'), 0)
+            easy_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Easy'), 0)
+            medium_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Medium'), 0)
+            hard_solved = next((item['count'] for item in ac_submission_num if item['difficulty'] == 'Hard'), 0)
+            lis.append(easy_solved)
+            lis.append(medium_solved)
+            lis.append(hard_solved)
+            lis.append(total_solved)
+            return lis
 
 
-
-print(my_data('Kigozi_Eria'))
